71c81cd9dac28fab388a9f3306fc5c8eabfbca49
Ty <tydresden2@huskers.unl.edu>
2025-08-28 11:24:24 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * Ty Dresden
+ * 2025-08-26
+ * To print out "Hello World!" to the standard output
+ */
+
+int main(int argc, char **argv) {
+
+    printf("Ty\n");
+
+    return 0;
+}
